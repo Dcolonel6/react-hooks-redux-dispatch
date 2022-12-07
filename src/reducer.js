1e6c97c1b9bdc6a45f433a421284b9072ac838b7
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-12-08 02:18:38 +0300

    followed along

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,6 @@
-function changeState(state, action) {
+let state = { count: 0 };
+
+function changeState(state = { count: 0 }, action) {
   switch (action.type) {
     case "counter/increment":
       return { count: state.count + 1 };
@@ -7,7 +9,13 @@ function changeState(state, action) {
   }
 }
 
-let state = { count: 0 };
-let action = { type: "counter/increment" };
+function render() {
+  document.body.textContent = state.count;
+}
 
-changeState(state, action);
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+
+//render();
